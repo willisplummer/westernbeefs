3fb4de31506d20a1979f7cd6a45f79439bcbd882
YOUR NAME <YOUR@EMAIL.com>
2015-06-04 01:03:11 -0400

    Revert "blog"
    This reverts commit 0dc02bc683ae8ef8434501cd984fe2aad2efe39e.

--- app/models/article.rb
+++
@@ -1,5 +0,0 @@
-class Article < ActiveRecord::Base
-	has_many :pages, dependent: :destroy
-	validates :title, presence: true,
-					  length: { minimum: 5 }
-end
--- app/models/page.rb
+++
@@ -1,4 +0,0 @@
-class Page < ActiveRecord::Base
-	belongs_to :article
-
-end
