95d5ddad9ca1c93e72451011b0611c2c3e654b9a
Willis Plummer <willisplummer@gmail.com>
2015-09-04 17:38:40 -0400

    polymorphic stories work now, need to be refactored still

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,24 +12,34 @@ class PagesController < ApplicationController
   	end
 
 	def edit
-		@article = Article.find(params[:article_id])
   		@page = @paginable.pages.find(params[:id])
   	end
 
 	def create
 		@page = @paginable.pages.create(page_params)
-		redirect_to article_admin_path(@article)
+    	if @page.paginable_type == "Article"
+			redirect_to article_admin_path(id: @paginable)
+	    elsif @page.paginable_type == "Story"
+	    	redirect_to article_story_admin_path(article_id: @paginable.article, id: @paginable)
+	    end
 	end
 
 	def update
-		@article = Article.find(params[:article_id])
     	@page = @paginable.pages.find(params[:id])
 
-    	if @page.update(page_params)
-      		redirect_to article_admin_path(@article)
-   		else
-      		render 'edit'
-    	end
+    	if @page.paginable_type == "Article"
+	    	if @page.update(page_params)
+	      		redirect_to article_admin_path(id: @paginable)
+	   		else
+	      		render 'edit'
+	    	end
+	    elsif @page.paginable_type == "Story"
+	    	if @page.update(page_params)
+	      		redirect_to article_story_admin_path(article_id: @paginable.article, id: @paginable)
+	   		else
+	      		render 'edit'
+	    	end
+	    end
   	end
 
 	def destroy
--- app/models/page.rb
+++ app/models/page.rb
@@ -2,31 +2,43 @@ class Page < ActiveRecord::Base
 	belongs_to :paginable, polymorphic: true
 
 	def page_prev
-		if page_number == 1
-			nil
-		else
-			page_number - 1
+		if paginable_type == "Article"
+			if page_number == 1
+				"#{paginable.slug}"
+			else
+				prev_page = page_nav_zerod(page_number - 1)
+				"#{paginable.slug}/prev_page"
+			end
+		elsif paginable_type == "Story"
+			if page_number == 1
+				"#{paginable.article.slug}"
+			else
+				prev_page = page_nav_zerod(page_number - 1)
+				"#{paginable.article.slug}/#{paginable.slug}/#{prev_page}"
+			end
 		end
 	end
 
-	def page_prev_zerod
-		if page_prev == nil
-			nil
-		else
-			page_prev.to_s.rjust(2, '0')
-		end
+	def page_nav_zerod(p)
+		p.to_s.rjust(2, '0')
 	end
 
 	def page_next
-		if page_number == paginable.page_count
-			'bio'
-		else
-			page_number + 1
+		if paginable_type == "Article"
+			if page_number == paginable.page_count
+				"#{paginable.slug}/bio"
+			else
+				next_page = page_number + 1
+				"#{paginable.slug}/page_nav_zerod(next_page)"
+			end
+		elsif paginable_type == "Story"
+			if page_number == paginable.page_count
+				"#{paginable.article.slug}"
+			else
+				next_page = page_nav_zerod(page_number + 1)
+				"#{paginable.article.slug}/#{paginable.slug}/#{next_page}"
+			end
 		end
-	end
-
-	def page_next_zerod
-		page_next.to_s.rjust(2, '0')
 	end
 
 	def page_number_zerod
