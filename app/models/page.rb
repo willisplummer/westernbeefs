49d5df38a69a04d10ad7cf9901363717ece56854
Willis Plummer <willisplummer@gmail.com>
2015-09-04 21:50:26 -0400

    fixed article_page nav issues

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,13 +1,17 @@
 class Page < ActiveRecord::Base
 	belongs_to :paginable, polymorphic: true
 
+	def page_nav_zerod(p)
+		p.to_s.rjust(2, '0')
+	end
+
 	def page_prev
 		if paginable_type == "Article"
 			if page_number == 1
 				"#{paginable.slug}"
 			else
 				prev_page = page_nav_zerod(page_number - 1)
-				"#{paginable.slug}/prev_page"
+				"#{paginable.slug}/#{prev_page}"
 			end
 		elsif paginable_type == "Story"
 			if page_number == 1
@@ -19,22 +23,19 @@ class Page < ActiveRecord::Base
 		end
 	end
 
-	def page_nav_zerod(p)
-		p.to_s.rjust(2, '0')
-	end
-
 	def page_next
 		if paginable_type == "Article"
 			if page_number == paginable.page_count
 				"#{paginable.slug}/bio"
 			else
-				next_page = page_number + 1
-				"#{paginable.slug}/page_nav_zerod(next_page)"
+				next_page = page_nav_zerod(page_number + 1)
+				"#{paginable.slug}/#{next_page}"
 			end
 		elsif paginable_type == "Story"
 			if page_number == paginable.page_count
 				"#{paginable.article.slug}"
 			else
+				page_number + 1
 				next_page = page_nav_zerod(page_number + 1)
 				"#{paginable.article.slug}/#{paginable.slug}/#{next_page}"
 			end
