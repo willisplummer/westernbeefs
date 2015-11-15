2237209c07c348df5e636b98bd2f8d5430403555
Willis Plummer <willisplummer@gmail.com>
2015-11-15 14:07:02 -0500

    facebook og metatags

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,11 @@
 module ArticlesHelper
+	def get_url(article, page, bio)	
+		if page
+			page.paginable_type == "Article" ? article_page_url(article, page) : story_page_url(article, page.paginable, page)
+		elsif bio
+			article_bio_url(article)
+		else
+			article_url(article)
+		end
+	end
 end
